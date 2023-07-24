5b0ed8c89b3d114db130317101cb7ff0c775f82f
seannn9 <naeskirlu9@gmail.com>
2023-07-24 18:01:24 +0800

    added some functions

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 # Text based tic tac toe
+board = [" " for i in range(10)]
+
+def drawBoard(board):
+    print("    |    |")
+    print("  " + board[1] + " | " + board[2] + "  | " + board[3])
+    print("    |    |")
+    print("  ----------")
+    print("    |    |")
+    print("  " + board[4] + " | " + board[5] + "  | " + board[6])
+    print("    |    |")
+    print("  ----------")
+    print("    |    |")
+    print("  " + board[7] + " | " + board[8] + "  | " + board[9])
+    print("    |    |")
+
+def player():
+    place = int(input('1-9: '))
+    board[place] = 'X'
+    return place
+
+def comp(player):
+    if board[player-1] == ' ' or board[player+1] == ' ':
+        board[1] = 'O'
 
 def main():
-    pass
+    comp(player())
+    drawBoard(board)
 
 if __name__ == '__main__':
     main()
